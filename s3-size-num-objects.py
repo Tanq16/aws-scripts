7895349c6f08feff77eb384feafbdb5806d39041
Tanishq Rupaal <tanishq.rupaal@praetorian.com>
2023-04-02 03:14:42 -0500

    removed executor in favor single by single script; non-standard

--- s3-size-num-objects.py
+++ s3-size-num-objects.py
@@ -1,5 +1,5 @@
-# S3 Size & Number of Objects per Bucket
-# --------------------------------------
+# S3 Number of Objects per Bucket
+# -------------------------------
 # This script takes in the value of a profile that has an
 # active session locally and prints out the name of all
 # buckets within that account and a count of the number
@@ -7,32 +7,7 @@
 
 import sys
 import boto3
-import threading
-
-threadLimiter = threading.BoundedSemaphore(24)
-
-class BucketCounterThread(threading.Thread):
-    def __init__(self, bucket_name, sess):
-        threading.Thread.__init__(self)
-        self.bucket_name = bucket_name
-        self.sess = sess
-
-    def run(self):
-        threadLimiter.acquire()
-        try:
-            self.run_limited()
-        finally:
-            threadLimiter.release()
-
-    def run_limited(self):
-        s3 = self.sess.resource('s3')
-        bucket = s3.Bucket(self.bucket_name)
-        count, size = 0, 0
-        for obj in bucket.objects.all():
-            count += 1
-            size += obj.size
-        size = convert_size(size)
-        print(f"{size} ({count})  :  {self.bucket_name}")
+import concurrent.futures
 
 def convert_size(size_bytes):
     for unit in ['B', 'KB', 'MB', 'GB', 'TB']:
@@ -41,19 +16,22 @@ def convert_size(size_bytes):
         size_bytes /= 1024.0
     return f"{size_bytes:.2f} {unit}"
 
+def run_enumerator(bucket_name,sess):
+    s3 = sess.resource('s3')
+    bucket = s3.Bucket(bucket_name)
+    count, size = 0, 0
+    for obj in bucket.objects.all():
+        count += 1
+        size += obj.size
+    size = convert_size(size)
+    print(f"{size} ({count})  :  {bucket_name}")
+
 def main(profile):
     session = boto3.Session(profile_name=profile)
     s3 = session.resource('s3')
     bucket_names = [bucket.name for bucket in s3.buckets.all()]
-
-    threads = []
     for bucket_name in bucket_names:
-        t = BucketCounterThread(bucket_name, session)
-        threads.append(t)
-        t.start()
-
-    for t in threads:
-        t.join()
+        run_enumerator(bucket_name, session)
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
